1c393b3728b5c43a652824ca31002b1e2d6c9dd3
Dipinjit Hanspal <hanspal.singhd@gmail.com>
2021-09-07 21:54:25 -0400

    Refactor to-do sub project

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ToDoModule } from '../to-do/to-do.module';
+import { ToDoModule } from './to-do/to-do.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
