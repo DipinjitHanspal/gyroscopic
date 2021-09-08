8d72cf42e8e4e5b5e83c9e97243f6e509607fbab
Dipinjit Hanspal <hanspal.singhd@gmail.com>
2021-09-07 21:54:49 -0400

    Add mongoose integration

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { ToDoModule } from './to-do/to-do.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [ToDoModule],
+  imports: [
+    ToDoModule,
+    MongooseModule.forRoot('mongodb+srv://clientServer:Gyroscopic@devcluster.ebjxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
